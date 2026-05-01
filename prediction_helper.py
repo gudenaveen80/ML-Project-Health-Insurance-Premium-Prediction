23427584639e5776061412e5baf9776ca43fb472
gudenaveen80 <gudenaveen80@gmail.com>
2026-05-01 18:20:25 +0530

    Fix file paths for Linux deployment

--- prediction_helper.py
+++ prediction_helper.py
@@ -3,10 +3,10 @@
 import pandas as pd
 import joblib
 
-model_young = joblib.load("artifacts\model_young.joblib")
-model_rest = joblib.load("artifacts\model_rest.joblib")
-scaler_young = joblib.load("artifacts\scaler_young.joblib")
-scaler_rest = joblib.load("artifacts\scaler_rest.joblib")
+model_rest  = load("artifacts/model_rest.joblib")
+model_young = load("artifacts/model_young.joblib")
+scaler_rest  = load("artifacts/scaler_rest.joblib")
+scaler_young = load("artifacts/scaler_young.joblib")
 
 def calculate_normalized_risk(medical_history):
     risk_scores = {
